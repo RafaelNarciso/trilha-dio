33d8ad77c28b9f0b3665250c979d82cba5817fa6
RafaelNarciso <rafaelnarcisodasilva@gmail.com>
2024-07-04 19:04:42 -0300

    first-commit

---
+++ ATIVIDADES/src/exercicios/ExemploWhile.java
@@ -0,0 +1,25 @@
+package exercicios;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ExemploWhile {
+    public static void main(String[] args) throws Exception {
+        double mesada = 50.0;
+
+        while (mesada > 0) {
+            Double valorDoce = valorAletorio();
+            if (valorDoce > mesada)
+                valorDoce = mesada;
+
+            System.out.println("Doce do valor: " + String.format("%.2f", valorDoce) + " Adicionado no carrinho ");
+            mesada = mesada - valorDoce;
+        }
+        System.out.println("Mesada : " + mesada);
+        System.out.println("Gastou seu todo seu dinheiro em doces ..");
+
+    }
+
+    private static double valorAletorio() {
+        return ThreadLocalRandom.current().nextDouble(2, 8);
+    }
+}
